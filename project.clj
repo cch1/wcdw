e3eb66337595ef79df9e2477e23f5da1e13f8740
Paul Bostrom <pbostrom@Pauls-MacBook-Pro.local>
2014-02-17 09:05:08 -0600

    Add tmp directory and use for java.io.tmpdir

--- project.clj
+++ project.clj
@@ -8,14 +8,14 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.datomic/datomic-free "0.9.4556" :exclusions [org.slf4j/slf4j-nop
                                                                    org.slf4j/log4j-over-slf4j]]
-                 [org.clojure/tools.logging "0.2.6"]
-                 [org.slf4j/slf4j-api "1.7.6"]
-                 [org.slf4j/jcl-over-slf4j "1.7.6"]
-                 [org.slf4j/slf4j-log4j12 "1.7.6"]
-                 [log4j/log4j "1.2.17"]]
+                 [org.clojure/tools.logging "0.2.6"]]
   :jvm-opts ["-server" "-Dlog4j.debug=false" "-Xms256M" "-Xmx1g"
              "-Djava.io.tmpdir=./tmp"] ;; This ensures resources/log4j.properties works as designed.
   :profiles {:dev {:resource-paths ["dev-resources"]
                    :source-paths ["util"]
-                   :dependencies [[midje "1.6.2"]]}}
+                   :dependencies [[midje "1.6.2"]
+                                  [org.slf4j/slf4j-api "1.7.6"]
+                                  [org.slf4j/jcl-over-slf4j "1.7.6"]
+                                  [org.slf4j/slf4j-log4j12 "1.7.6"]
+                                  [log4j/log4j "1.2.17"]]}}
   :reload-paths ["src"])
